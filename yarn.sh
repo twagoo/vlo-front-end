215c9441349e0b177f92d3ed260df48c5a3fbce0
Twan Goosen <twan@clarin.eu>
2023-02-21 12:11:08 +0200

    initiated project

--- yarn.sh
+++ yarn.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-IMAGE="vlo-node:latest"
+IMAGE='vlo-node:latest'
+CONTAINER_NAME='vlo-yarn'
+ENTRYPOINT='yarn'
+PORT_MAPPING='3000:3000'
 
 if ! docker images -q "${IMAGE}"|egrep '.*' > /dev/null; then
 	echo "Image '${IMAGE}' not found. Need to build first?"
@@ -7,4 +10,11 @@ if ! docker images -q "${IMAGE}"|egrep '.*' > /dev/null; then
 fi
 
 SCRIPT_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
-docker run -ti --rm -v "${PWD}:${PWD}" -w "${PWD}" "${IMAGE}" "$@"
+docker run -ti --rm \
+	--name "${CONTAINER_NAME}" \
+	-v "vlo-node-yarn-cache:/usr/local/share/.cache/yarn" \
+	-v "${PWD}:${PWD}" \
+	-w "${PWD}" \
+	-p "${PORT_MAPPING}" \
+	--entrypoint "${ENTRYPOINT}" \
+	"${IMAGE}" "$@"
