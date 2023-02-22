fcbba9799e5580ea799bbb293aaacc950c4c410e
Twan Goosen <twan@clarin.eu>
2023-02-22 11:40:14 +0200

    pass q param to service request

--- pages/records.js
+++ pages/records.js
@@ -1,5 +1,7 @@
-const Records = ({records}) => {
-    const recordsList = records.map(record =>{
+const SERVICE_BASE_URL = 'http://localhost:8708';
+
+function Records({ records }) {
+    const recordsList = records.map(record => {
         return <div key={record.id}>
             <h2>{record.fields.name}</h2>
             <p>{record.fields.description}</p>
@@ -14,12 +16,24 @@ const Records = ({records}) => {
     )
 }
 
-Records.getInitialProps = async () => {
-    const res = await fetch('http://localhost:8708/records');
+function requestUrlForContext(ctx) {
+    const reqParams = {};
+    const q = ctx.query['q'];
+    if (q) {
+        reqParams.q = q;
+    }
+
+    return SERVICE_BASE_URL+'/records?' + new URLSearchParams(reqParams);
+}
+
+Records.getInitialProps = async (ctx) => {
+    const reqUrl = requestUrlForContext(ctx);
+
+    const res = await fetch(reqUrl);
     const json = await res.json();
     return {
         records: json
     }
-  };
+};
 
 export default Records;
