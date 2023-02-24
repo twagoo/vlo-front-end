ebbc9f9d11063dbf0d1ee89add6412fc58d94b6f
Twan Goosen <twan@clarin.eu>
2023-02-24 15:51:55 +0200

    redirect root to /search

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/search',
+        permanent: true,
+      },
+    ]
+  },
+}
--- pages/_document.js
+++ pages/_document.js
@@ -5,15 +5,15 @@ export default function Document() {
     <Html lang="en">
       <Head>
         { /* TODO: bundled */ }
-      <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossorigin></script>
-      <script src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js" crossorigin></script>
-      <script src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js" crossorigin></script>
+      <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossOrigin='anonymous'></script>
+      <script src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js" crossOrigin='anonymous'></script>
+      <script src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js" crossOrigin='anonymous'></script>
       <script>var Alert = ReactBootstrap.Alert;</script>
       <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-        crossorigin="anonymous" />
+        crossOrigin="anonymous" />
       </Head>
       <body>
         <Main />
