ea38299bad23d4b41330c684c544c82ffd173f86
Twan Goosen <twan@clarin.eu>
2023-03-14 17:36:17 +0200

    read and apply environment variables for API URL and log level at runtime

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
-const DEFAULT_LOG_LEVEL = 'INFO'
 const DEV_LOG_LEVEL = 'DEBUG'
 
 module.exports = async (phase, { defaultConfig }) => {
   /** @type {import('next').NextConfig} */
   const nextConfig = {
     serverRuntimeConfig: {
-      logLevel: determineLogLevel(phase),
-      vloServiceBaseUrl: process.env.VLO_SERVICE_BASE_URL,
+      logLevel: determineLogLevel(phase)
     },
     reactStrictMode: true,
     async redirects() {
@@ -30,14 +28,11 @@ module.exports = async (phase, { defaultConfig }) => {
 }
 
 function determineLogLevel(phase) {
-  if (process.env.VLO_FRONT_END_LOG_LEVEL) {
-    console.log('Logging at configured', process.env.VLO_FRONT_END_LOG_LEVEL, 'level - overriding default', DEFAULT_LOG_LEVEL);
-    return process.env.VLO_FRONT_END_LOG_LEVEL;
-  } else if (phase === PHASE_DEVELOPMENT_SERVER) {
-    console.log('Development server phase detected - logging at', DEV_LOG_LEVEL, 'level - overriding default', DEFAULT_LOG_LEVEL);
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    console.log('Development server phase detected - logging at', DEV_LOG_LEVEL, 'level - overriding default');
     return DEV_LOG_LEVEL;
   } else {
-    return DEFAULT_LOG_LEVEL;
+    return null;
   }
 }
 
--- util/logging.js
+++ util/logging.js
@@ -2,10 +2,17 @@ import _log from 'loglevel';
 import getConfig from 'next/config';
 
 const { serverRuntimeConfig } = getConfig();
-if (serverRuntimeConfig.logLevel) {
+
+const envLogLevel = process.env.VLO_FRONT_END_LOG_LEVEL;
+if (envLogLevel) {
+    console.log('Logging at [', envLogLevel, '] level as configured in environment (VLO_FRONT_END_LOG_LEVEL)- overriding any default');
+    _log.setLevel(envLogLevel);
+} else if (serverRuntimeConfig.logLevel) {
     const logLevel = serverRuntimeConfig.logLevel;
-    _log.info('Setting log level to', logLevel, 'as configured');
+    _log.info('Setting log level to [', logLevel, '] as per NextJS configuration');
     _log.setLevel(logLevel);
+} else {
+    console.log('No log level configured, using loglevel library defaults');
 }
 
 export default _log;
