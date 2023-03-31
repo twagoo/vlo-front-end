83b17473b7c4e07700ce951548c6b87d9b7106ea
Twan Goosen <twan@clarin.eu>
2023-03-31 17:14:55 +0300

    unit test

--- service/ParameterConverter.test.js
+++ service/ParameterConverter.test.js
@@ -4,24 +4,27 @@ import { facetSelectionMapToFq } from './ParameterConverter'
 loglevel.setLevel('debug');
 jest.mock('@/util/logging', () => loglevel);
 
-test('empty selection', () => {
-    const selection = {};
-    const expected = [];
-    const result = facetSelectionMapToFq(selection);
-    expect(result).toEqual(expected);
-});
+describe('facet selection to fq mapping', () => {
 
-test('simple selection', () => {
-    const selection = { 'fieldA': ['valueA'], 'fieldB': 'valueB' };
-    const expected = ['fieldA:valueA', 'fieldB:valueB'];
-    const result = facetSelectionMapToFq(selection);
-    expect(result).toEqual(expected);
-});
+    test('empty selection', () => {
+        const selection = {};
+        const expected = [];
+        const result = facetSelectionMapToFq(selection);
+        expect(result).toEqual(expected);
+    });
+
+    test('simple selection', () => {
+        const selection = { 'fieldA': ['valueA'], 'fieldB': 'valueB' };
+        const expected = ['fieldA:valueA', 'fieldB:valueB'];
+        const result = facetSelectionMapToFq(selection);
+        expected.forEach((expectedValue) => expect(result).toContain(expectedValue));
+    });
 
 
-test('multiple selection', () => {
-    const selection = { 'fieldA': ['valueA1', 'valueA2'], 'fieldB': 'valueB' };
-    const expected = ['fieldA:valueA1', 'fieldA:valueA2', 'fieldB:valueB'];
-    const result = facetSelectionMapToFq(selection);
-    expect(result).toEqual(expected);
+    test('multiple selection', () => {
+        const selection = { 'fieldA': ['valueA1', 'valueA2'], 'fieldB': 'valueB' };
+        const expected = ['fieldA:valueA1', 'fieldA:valueA2', 'fieldB:valueB'];
+        const result = facetSelectionMapToFq(selection);
+        expected.forEach((expectedValue) => expect(result).toContain(expectedValue));
+    });
 });
